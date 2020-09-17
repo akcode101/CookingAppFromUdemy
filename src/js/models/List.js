e6167eb53e88164a3621f15ccb1987102b74d9d6
akcode101 <akale2009@gmail.com>
2020-09-17 15:29:50 -0700

    Cooking app from udemy

---
+++ src/js/models/List.js
@@ -0,0 +1,35 @@
+import uniqid from 'uniqid';
+
+export default class List 
+{
+	constructor ()
+	{
+		this.items= [];
+	}
+
+	addItem (count, unit, ingredient)
+	{
+		const item= {
+			id: uniqid(),
+			count, 
+			unit, 
+			ingredient
+		}
+		this.items.push(item);
+		return item;
+	}
+
+	//slice creates a new array, splice mutates original
+
+	deleteItem(id)
+	{
+		const index= this.items.findIndex(el => el.id===id);
+
+		this.items.splice(index, 1); //only remove one element
+	}
+
+	updateCount(id, newCount)
+	{
+		this.items.find(el => el.id===id).count= newCount;
+	}
+}
